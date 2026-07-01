56fbeb00eed8239b644cf38af6026a449ca1f90d
aobowers03 <146280262+aobowers03@users.noreply.github.com>
2026-06-30 23:56:33 -0400

    Refacotring files based on directory name change

---
+++ bitewise-frontend/src/pages/PantryPage.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from "../components/Navbar";
+
+function PantryPage() {
+  return (
+    <section className="screen" id="pantry">   
+       <div>
+        <Navbar />
+        <h1>Pantry Page</h1>
+        {/* Add your pantry page content here */}
+        </div>
+    </section>
+  );
+}
+
+export default PantryPage;
