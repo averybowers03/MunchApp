4643d41d42697aa65cb02122f4c30663a044556b
aobowers03 <146280262+aobowers03@users.noreply.github.com>
2026-07-01 00:11:44 -0400

    Updating pages/fixed small bug

--- bitewise-frontend/src/pages/PantryPage.tsx
+++ bitewise-frontend/src/pages/PantryPage.tsx
@@ -6,7 +6,8 @@ function PantryPage() {
     <section className="screen" id="pantry">   
        <div>
         <Navbar />
-        <h1>Pantry Page</h1>
+        <h1>My Pantry</h1>
+        <small>*blank* items available</small>
         {/* Add your pantry page content here */}
         </div>
     </section>
