c70c53217b37739f756e28ad2eed526628348fa1
aobowers03 <146280262+aobowers03@users.noreply.github.com>
2026-07-10 13:13:00 +1000

    Made the queries dynamic for autocomplete search functions

--- bitewise-backend/src/index.ts
+++ bitewise-backend/src/index.ts
@@ -14,7 +14,8 @@ const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/api/recipes', (req, res) => {
-  fetch('https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,flour,sugar&number=5&ranking=1&ignorePantry=true&apiKey=' + process.env.SPOONACULAR_API_KEY) // Replace
+  const { query } = req.query;
+  fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${query}&number=5&ranking=1&ignorePantry=true&apiKey=` + process.env.SPOONACULAR_API_KEY) // Replace
     .then(response => response.json())
     .then(data => res.json(data))
     .catch(error => {
@@ -24,7 +25,8 @@ app.get('/api/recipes', (req, res) => {
 });
 
 app.get('/api/ingredients', (req, res) => {
-  fetch('https://api.spoonacular.com/food/ingredients/autocomplete?query=appl&number=5&apiKey=' + process.env.SPOONACULAR_API_KEY)
+  const { query } = req.query;
+  fetch(`https://api.spoonacular.com/food/ingredients/autocomplete?query=${query}&number=5&apiKey=` + process.env.SPOONACULAR_API_KEY)
     .then(response => response.json())
     .then(data => res.json(data))
     .catch(error => {
