666dbeb7057f66960a5a231bb41a5b09fbd1ed9e
aobowers03 <146280262+aobowers03@users.noreply.github.com>
2026-07-13 11:08:21 +1000

    Attained a working debouncer. Still needs API connection

--- bitewise-frontend/src/components/IngredientSearch.tsx
+++ bitewise-frontend/src/components/IngredientSearch.tsx
@@ -1,16 +1,36 @@
 
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { PantryContext } from '../context/PantryContext';
 
 function IngredientSearch() {
     
   const { ingredients, setIngredients, addIngredient } = useContext(PantryContext);
 
+  const [query , setQuery] = useState('');
+  const [suggestions, setSuggestions] = useState(query);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setSuggestions(query);
+    }, 500);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [query]);
+
+  useEffect(() => {
+    if (suggestions.length > 0) {
+      console.log('Suggestions:', suggestions);
+    }
+  }, [suggestions]);
+
     return (
+
         <div className="pantry-search">
 
         <form className="search-form">
-          <input value={ingredients.ingredient} onChange={(e) => setIngredients({ ...ingredients, ingredient: e.target.value })} type="text" className="ingredient-input" placeholder="Enter ingredient..." />
+          <input value={query} onChange={(e) => setQuery(e.target.value)} type="text" className="ingredient-input" placeholder="Enter ingredient..." />
           <input value={ingredients.qty} onChange={(e) => setIngredients({ ...ingredients, qty: e.target.value })} type="text" className="qty-input" placeholder="Qty" />
 
           <select className="unit-select" value={ingredients.unit} onChange={(e) => setIngredients({ ...ingredients, unit: e.target.value })}>
