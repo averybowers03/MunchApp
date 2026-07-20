c33ecb979597507ed2b0b6eed8d0452c076c9bac
aobowers03 <295009350+averybowers03@users.noreply.github.com>
2026-07-20 13:19:35 +1000

    fix: prompt the API for the correct data

--- munch-backend/src/index.ts
+++ munch-backend/src/index.ts
@@ -39,7 +39,7 @@ app.get('/api/ingredients', (req, res) => {
   const { query, ingList } = req.query;
   fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&includeIngredients=${ingList}&fillIngredients=true&number=100&ignorePantry=true&apiKey=` + process.env.SPOONACULAR_API_KEY) // Replace
     .then(response => response.json())
-    .then(data => res.json(data))
+    .then(data => res.json(data.results))
     .catch(error => {
       console.error('Error fetching recipes:', error);
       res.status(500).json({ error: 'Failed to fetch recipes' });
@@ -48,9 +48,9 @@ app.get('/api/ingredients', (req, res) => {
 
 app.get('/api/recipes/randomSearch', (req, res) => {
   const { query } = req.query;
-  fetch(`https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${query}&fillIngredients=true&number=100&ignorePantry=true&sort=random&apiKey=` + process.env.SPOONACULAR_API_KEY) // Replace
+  fetch(`https://api.spoonacular.com/recipes/complexSearch?includeIngredients=garlic,cheese&fillIngredients=true&addRecipeInformation=true&&number=100&ignorePantry=true&sort=random&apiKey=` + process.env.SPOONACULAR_API_KEY) // Replace
     .then(response => response.json())
-    .then(data => res.json(data))
+    .then(data => res.json(data.results))
     .catch(error => {
       console.error('Error fetching recipes:', error);
       res.status(500).json({ error: 'Failed to fetch recipes' });
