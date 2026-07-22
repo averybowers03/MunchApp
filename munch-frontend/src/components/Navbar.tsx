a57aeeee78de34801913dcd7b26d35ba561815f9
aobowers03 <295009350+averybowers03@users.noreply.github.com>
2026-07-22 12:37:30 +1000

    style: correct name of app in navbar

--- munch-frontend/src/components/Navbar.tsx
+++ munch-frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (
       
         <nav className="app-nav">
-          <div className="logo"><span className="dot"></span>Bitewise</div>
+          <div className="logo"><span className="dot"></span>Munch</div>
           <div className="nav-buttons">
             <Link to="/pages/Dashboard" className="dashboard-link">Dashboard</Link>
             <Link to="/pages/PantryPage" className="pantry-link">Pantry</Link>
