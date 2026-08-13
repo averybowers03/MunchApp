5cbfe473117850dd2454ce3e07429c88c7425984
aobowers03 <295009350+averybowers03@users.noreply.github.com>
2026-08-13 10:15:27 +1000

    style: adds an svg close button that will fade to red when hovered over

--- munch-frontend/src/pages/PantryPage.tsx
+++ munch-frontend/src/pages/PantryPage.tsx
@@ -23,7 +23,14 @@ function PantryPage() {
          {ingredientList.map((item, index) => (
            <div className="pantry-item" key={index}>
              <span>{item.ingredient} - {item.qty} {item.unit}</span>
-             <button className="remove-btn" onClick={() => removeIngredient(index)}>x</button>
+             <button className="remove-btn" onClick={() => removeIngredient(index)}>
+               <svg width="25px" height="25px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+               <rect x="0" fill="none" width="20" height="20"/>
+               <g>
+                <path d="M14.95 6.46L11.41 10l3.54 3.54-1.41 1.41L10 11.42l-3.53 3.53-1.42-1.42L8.58 10 5.05 6.47l1.42-1.42L10 8.58l3.54-3.53z"/>
+                </g>
+                </svg>
+              </button>
            </div>
          ))}
          
