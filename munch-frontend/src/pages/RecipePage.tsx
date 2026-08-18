47b3fdbe7511a43d8c527c56b82157fce21275d1
aobowers03 <295009350+averybowers03@users.noreply.github.com>
2026-08-18 13:23:15 +1000

    [feat] creates and styles the recipe page to display selected recipe's info

--- munch-frontend/src/pages/RecipePage.tsx
+++ munch-frontend/src/pages/RecipePage.tsx
@@ -1,21 +1,111 @@
 
-import { RecipeContext} from '../context/RecipeContext';
-import {  useContext } from 'react';
+import { RecipeContext } from '../context/RecipeContext';
+import {  useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import "./RecipePage.css";
 
 function RecipePage() {
 
-//const { id } = useParams();
+const { id } = useParams();
 
-//const { recipes, setRecipes } = useContext(RecipeContext);
+const {recipes, setRecipes} = useContext(RecipeContext)
+
+
+useEffect(() => {
+      
+     fetch(`/api/recipes/searchById?id=${716429}`)
+      .then(res => res.json())
+      .then(data => setRecipes(data))
+      .catch(error => {
+      console.error('Error fetching ingredients:', error)}); 
+    
+      fetch(`/api/recipes/getInstructions?id=${id}`)
+      .then(res => res.json())
+      .then(data => setRecipes(prev => ({ ...prev, analyzedInstructions: data })))
+      .catch(error => {
+      console.error('Error fetching ingredients:', error)});
+      
+    }, [id]);
 
     return(
         <section className='screen'>
-            {/*{recipes.map(recipe => (
-                <div>
 
+            <div className='header-grid'>
+
+                <div className='recipe-header'>
+                    <h1>{recipes.title}</h1>
+                    <span className='srcName'>by {recipes.sourceName}</span>
+                    <a className='srcUrl' href={recipes.sourceUrl} target="_blank" rel="noopener noreferrer">
+                        View Original Recipe
+                    </a>
+                    <div className='meta-grid'>
+                        <span ><b>Prep</b>  -  {recipes.preparationMinutes} mins</span>
+                        <span className='cook-time'> <b>Cook</b>  -  {recipes.cookingMinutes} mins</span>
+                        <span> <b>Total</b>  -  {recipes.readyInMinutes} mins</span>
+                    </div>
                 </div>
-            ))}*/}
-            <div>info</div>
+
+                <div className='img'>
+                    <img src={recipes.image} alt={recipes.title} />
+                </div>
+
+            </div>
+
+            <div className='recipe-grid'>
+
+                <div className='summary'>
+                        <p>{recipes.summary}</p>
+                </div>
+
+                <div className='nutrition'>
+                    <h2>Nutrition</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nutrient</th>
+                                <th>Amount</th>
+                                <th>% Daily Values</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {recipes.nutrition.nutrients?.map((nutrient, i) => (
+                            <tr key={i}>
+                                <td>{nutrient.name}</td>
+                                <td>{nutrient.amount}{nutrient.unit}</td>
+                                <td>{nutrient.percentOfDailyNeeds}%</td>
+                            </tr>
+                            ))}
+                        </tbody>
+                        
+                    </table>
+                </div>
+
+                <div className='ingredient-list'>
+                    <h2>Ingredients</h2>
+                    <ul>
+                        {/*<li>sum</li>
+                        <li>sum else</li>
+                        {recipes.missedIngredients.map((ingredient, i) => (
+                        <li key={i}>{ingredient.name}</li>
+                        ))}*/}
+                    </ul>
+                </div>
+
+                <div className='instructions'>
+                    <h2>Instructions</h2>
+                
+                {recipes.analyzedInstructions.map((section, i) => (
+                    <div key={i}>
+                        {section.name && <h3>{section.name}</h3>}
+                        <ol>
+                            {section.steps.map(step => (
+                                <li key={step.number}>{step.step}</li>
+                                ))}
+                        </ol>
+                    </div>
+                ))}
+                </div>
+            </div>
         </section>
         
     )
