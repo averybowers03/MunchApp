2a204b2261d47a47290e157a8f9f4afbd5f37130
aobowers03 <295009350+averybowers03@users.noreply.github.com>
2026-09-01 12:11:17 +1000

    [feat] (WIP) develop the bookmarks page so as to get an idea about UI

--- munch-frontend/src/pages/BookmarksPage.tsx
+++ munch-frontend/src/pages/BookmarksPage.tsx
@@ -3,7 +3,66 @@ function BookmarksPage () {
 
     return (
 
-        <div><h1>Bookmarks Page</h1></div>
+        <section className="bookmarks-screen">
+
+            <div className="bkmrk-header">
+                <h1>Bookmarked</h1>
+                <select className="bkmrk-sort">
+                    <option>earliest</option>
+                    <option>latest</option>
+                </select> 
+            </div>
+            
+            <small className="bkmrk-length">## recipes saved</small>
+            <div className="bookmark-grid">
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+                <div className="bookmarked-recipe">
+                    <div className="recipe-info">
+                        <h3>Title</h3>
+                        <span>recipe info</span>
+                    </div>
+                    <img src="https://spoonacular.com/recipeImages/716429-312x231.jpg" alt=""/>
+                </div>
+            </div>
+            
+        </section>
+        
     );
 }
  export default BookmarksPage;
